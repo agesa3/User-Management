8f5431cf52592da3941c725c00a0c4e9d7cd0d92
agesa3 <collinsagesa2000@gmail.com>
2022-01-01 02:23:24 +0300

    setup the router

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser=require('body-parser')
 const app=express()
 const PORT=3000
 const connection=require('./config/database')
+const indexRouter=require('./routes/router')
 
 //set views
 app.set('views',path.join(__dirname,'views'))
@@ -13,6 +14,12 @@ app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
+
+//set public folder
+app.use(express.static(path.join(__dirname,'public')))
+
+//use router
+app.use("/user",indexRouter)
 
 
 app.listen(PORT,()=>{
