9d326463d618fd83f61c280399ee5686465c860e
agesa3 <collinsagesa2000@gmail.com>
2022-01-01 02:53:34 +0300

    able to fetch data from db and display on table

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,16 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const connection = require("../config/database");
 
 //render the index page
-router.get('/', (req, res) => {
-    res.render('index');
+router.get("/", (req, res) => {
+  let sql = "SELECT * FROM users";
+  let query = connection.query(sql, (err, results) => {
+    if (err) throw err;
+    res.render("index", {
+      title: "CRUD APP",
+      user: results,
+    });
+  });
 });
 
 module.exports = router;
