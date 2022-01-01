2ad301c6edfa992477d23961568e175741c3f3c3
agesa3 <collinsagesa2000@gmail.com>
2022-01-01 03:20:02 +0300

    able to post user

--- routes/router.js
+++ routes/router.js
@@ -13,4 +13,39 @@ router.get("/", (req, res) => {
   });
 });
 
+//router to add a new user
+// router.post("/add", (req, res) => {
+//   let post = {
+//     u_name: req.body.u_name,
+//     email: req.body.email,
+//     phone_number: req.body.phone_number,
+//   };
+//   let sql = "INSERT INTO users SET ?";
+//   let query = connection.query(sql, post, (err, results) => {
+//     if (err) throw err;
+//     res.redirect("/");
+//   });
+// });
+
+//router to render page to add new user and insert to database after submit
+router.get("/add", (req, res) => {
+  res.render("add", {
+    title: "Add User",
+  });
+});
+
+//save the user to database
+router.post("/save", (req, res) => {
+  let post = {
+    u_name: req.body.u_name,
+    email: req.body.email,
+    phone_number: req.body.phone_number,
+  };
+  let sql = "INSERT INTO users SET ?";
+  let query = connection.query(sql, post, (err, results) => {
+    if (err) throw err;
+    res.redirect("/user/");
+  });
+});
+
 module.exports = router;
